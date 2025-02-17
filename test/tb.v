ec3aee755ac8b0b702392b365808ef0ce01ab757
Monish-VR <119118249+Monish-VR@users.noreply.github.com>
2025-02-18 05:18:10 +0530

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -45,45 +45,5 @@ module tb ();
       .clk    (clk),      // clock
       .rst_n  (rst_n)     // not reset
   );
-always #5 clk = ~clk;
-    
-    initial begin
-        // Initialize signals
-        clk = 0;
-        rst_n = 0;
-        ena = 1;
-        ui_in = 0;
-        uio_in = 0;
-        
-        // Reset sequence
-        #10 rst_n = 1;
-        #10 rst_n = 0;
-        
-        // Write data to FIFO
-        ui_in[2] = 1;  // Write enable
-        ui_in[3] = 0;  // Read disable
-        ui_in[7:4] = 4'b1010; // Data to be written
-        #10 ui_in[2] = 0; // Disable write
-        
-        // Read data from FIFO
-        #20 ui_in[2] = 0;  // Write disable
-        ui_in[3] = 1;  // Read enable
-        #10 ui_in[3] = 0; // Disable read
-        
-        // Additional test cases
-        #20 ui_in[2] = 1; ui_in[7:4] = 4'b1100; #10 ui_in[2] = 0; // Write another data
-        #20 ui_in[3] = 1; #10 ui_in[3] = 0; // Read again
-        
-        // Finish simulation
-        #50;
-        $stop;
-    end
-    
-    // Monitor output
-    initial begin
-        $monitor("Time=%0t, Write=%b, Read=%b, Data In=%b, Data Out=%b, Full=%b, Empty=%b", 
-                 $time, ui_in[2], ui_in[3], ui_in[7:4], uo_out[5:2], uo_out[0], uo_out[1]);
-    end
 
-endmodule
 endmodule
