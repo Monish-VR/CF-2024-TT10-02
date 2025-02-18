6fcc91bc88786807bc1d50c398b44d48adf42664
Monish-VR <119118249+Monish-VR@users.noreply.github.com>
2025-02-18 05:43:34 +0530

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -28,7 +28,7 @@ module tb ();
 `endif
 
   // Replace tt_um_example with your module name:
-  tt_um_monishvr_fio (
+  tt_um_monishvr_fifo (
 
       // Include power ports for the Gate Level test:
 `ifdef GL_TEST
