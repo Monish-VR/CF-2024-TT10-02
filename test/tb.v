a3261e52f275baa41fc91da90b6977dbfb4bfd1c
Monish-VR <119118249+Monish-VR@users.noreply.github.com>
2025-02-18 06:05:20 +0530

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -23,8 +23,8 @@ module tb ();
   wire [7:0] uio_out;
   wire [7:0] uio_oe;
 `ifdef GL_TEST
-  // wire VPWR = 1'b1;
-  // wire VGND = 1'b0;
+  wire VPWR = 1'b1;
+  wire VGND = 1'b0;
 `endif
 
   // Replace tt_um_example with your module name:
@@ -32,8 +32,8 @@ module tb ();
 
       // Include power ports for the Gate Level test:
 `ifdef GL_TEST
-      .VPWR(1'b1),
-      .VGND(1'b0),
+      .VPWR(VPWR),
+     .VGND(VGND),
 `endif
 
       .ui_in  (ui_in),    // Dedicated inputs
