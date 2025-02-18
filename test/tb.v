b65a2dcf9b7d667ee114f2f6086745a3d8ba1fc9
Monish-VR <119118249+Monish-VR@users.noreply.github.com>
2025-02-18 21:52:24 +0530

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -1,73 +1,49 @@
-`timescale 1ns/1ps
+`default_nettype none
+`timescale 1ns / 1ps
 
-module tb();
+/* This testbench just instantiates the module and makes some convenient wires
+   that can be driven / tested by the cocotb test.py.
+*/
+module tb ();
 
-    // Testbench signals
-    reg [7:0] ui_in;
-    wire [7:0] uo_out;
-    reg [7:0] uio_in;
-    wire [7:0] uio_out;
-    wire [7:0] uio_oe;
-    reg clk, rst_n;
-    reg ena;
+  // Dump the signals to a VCD file. You can view it with gtkwave or surfer.
+  initial begin
+    $dumpfile("tb.vcd");
+    $dumpvars(0, tb);
+    #1;
+  end
 
+  // Wire up the inputs and outputs:
+  reg clk;
+  reg rst_n;
+  reg ena;
+  reg [7:0] ui_in;
+  reg [7:0] uio_in;
+  wire [7:0] uo_out;
+  wire [7:0] uio_out;
+  wire [7:0] uio_oe;
+// `ifdef GL_TEST
+//   wire VPWR = 1'b1;
+//   wire VGND = 1'b0;
+// `endif
+
+  // Replace tt_um_example with your module name:
+  tt_um_monishvr_fifo dut (
+
+      // Include power ports for the Gate Level test:
 `ifdef GL_TEST
       .VPWR(1'b1),
       .VGND(1'b0),
 `endif
-    // Instantiate the FIFO module
-    tt_um_monishvr_fifo dut (
-        .ui_in(ui_in),
-        .uo_out(uo_out),
-        .uio_in(8'b0),
-        .uio_out(),
-        .uio_oe(),
-        .ena(ena),
-        .clk(clk),
-        .rst_n(rst_n)
-    );
 
-    
-    
-    // Clock generation
-    always #5 clk = ~clk;
-    
-    initial begin
-        // Initialize signals
-        clk = 0;
-        rst_n = 0;
-        ui_in = 0;
-        uio_in = 0;
-        ena = 1;
-        
-        // Reset sequence
-        #10 rst_n = 1;
-        #10 rst_n = 0;
-        
-        // Write data to FIFO
-        ui_in[2] = 1;  // Write enable
-        ui_in[3] = 0;  // Read disable
-        ui_in[7:4] = 4'b1010; // Data to be written
-        #10 ui_in[2] = 0; // Disable write
-        
-        // Read data from FIFO
-        #20 ui_in[2] = 0;  // Write disable
-        ui_in[3] = 1;  // Read enable
-        #10 ui_in[3] = 0; // Disable read
-        
-        // Additional test cases
-        #20 ui_in[2] = 1; ui_in[7:4] = 4'b1100; #10 ui_in[2] = 0; // Write another data
-        #20 ui_in[3] = 1; #10 ui_in[3] = 0; // Read again
-        
-        // Finish simulation
-        #50;
-        $stop;
-    end
-    
-    // Monitor output
-    initial begin
-       $dumpfile("syncFifo.vcd");
-       $dumpvars(0, tb);    
-    end
+      .ui_in  (ui_in),    // Dedicated inputs
+      .uo_out (uo_out),   // Dedicated outputs
+      .uio_in (8'b0),   // IOs: Input path
+      .uio_out(),  // IOs: Output path
+      .uio_oe (),   // IOs: Enable path (active high: 0=input, 1=output)
+      .ena    (1'b1),      // enable - goes high when design is selected
+      .clk    (clk),      // clock
+      .rst_n  (rst_n)     // not reset
+  );
 
 endmodule
